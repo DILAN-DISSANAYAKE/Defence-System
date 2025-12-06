5795ed45fc5e3752b863f1a3b8e5f0deda1143a5
DILAN-DISSANAYAKE <dissanayakedilan2005@gmail.com>
2025-12-07 00:57:07 +0530

    added new add observable method in ControllerRoom class

--- src/controller/ControllerRoom.java
+++ src/controller/ControllerRoom.java
@@ -1,6 +1,25 @@
 
 package controller;
 
-public class ControllerRoom {
+import interfaces.Observable;
+import java.util.ArrayList;
+
+public class ControllerRoom extends javax.swing.JFrame{
     
+    private ArrayList<Observable> observvables=new ArrayList<>();
+    
+    public void addObservable(Observable observable){
+        this.observvables.add(observable);
+    }
+    
+    private Observable[] observable2=new Observable[0];
+    
+    public void addObservable2(Observable observerble){
+        Observable[] tempObservable=new Observable[observable2.length+1];
+        for (int i = 0; i < observable2.length; i++) {
+            tempObservable[i]=observable2[i];
+        }
+        tempObservable[tempObservable.length-1]=observerble;
+        observable2=tempObservable;
+    }
 }
